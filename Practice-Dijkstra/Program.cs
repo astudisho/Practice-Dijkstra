9adfa3d3ff13633d05632f94b40eef7739851a36
Astudillo <astudisho@gmail.com>
2018-01-21 23:55:30 -0600

    Add project files.

---
+++ Practice-Dijkstra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
